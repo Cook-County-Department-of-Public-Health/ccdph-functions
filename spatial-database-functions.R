12e823bc6e6d0f59a846f72c32cd8cbea786d313
Smith <christopher.smith@cookcountyhealth.org>
2023-05-18 09:05:19 -0500

    changes to spatial-database-functions.R script

--- spatial-database-functions.R
+++ spatial-database-functions.R
@@ -1,16 +1,27 @@
-#### Required packages ####
+# Header -----------------------------------------------------------
+
+# Script name: spatial-database-functions.R
+# Purpose: write spatial data to CCDPH SQL Server inter-spatial database
+# Author: C. Scott Smith (christopher.smith@cookcountyhealth.org)
+# Created: 5/15/2023 
+# Last Updated: 5/17/2023
+
+# Attach R packages -----------------------------------------------------
 
 library(sf)
+library(DBI)
+library(odbc)
 library(dplyr)
 library(tidyverse)
 library(spData)
 library(keyring)
+library(tigris)
+library(units)
 
-# set key for connecting to CCDPH SQL Server instance
+#Name of CCDPH SQL Server is encrypted. Set name using keyring's key_set function.
 #key_set("ccdph_sql_server")
 
-## process, add spatial data to inter-spatial database on CCDPH SQL Server
-fx_spatial_data_output <- function(sf_spatialdataset, layer_name, crs_id = 3435){
+fx_spatial_layer_to_database <- function(sf_layer_name, schema_name, db_table_name, crs_id){
   
   # connect to inter-spatial
   con_inter_spatial <- dbConnect(odbc::odbc(),
@@ -18,33 +29,32 @@ fx_spatial_data_output <- function(sf_spatialdataset, layer_name, crs_id = 3435)
                                  Server   = key_get("ccdph_sql_server"),
                                  Database = "inter-spatial")
   
-  sf_spatialdataset_df <- as.data.frame(sf_spatialdataset) # convert to data frame
-  sf_spatialdataset_geom <- sf_spatialdataset_df$geometry # isolate geometry column
-  sf_spatialdataset_df[,"geom"]<-st_as_text(st_transform(sf_spatialdataset_geom,crs = crs_id)) # convert geometry format
+  sf_layer_df <- as.data.frame(sf_layer_name) # convert to data frame
+  sf_layer_geom <- sf_layer_df$geometry # isolate geometry column
+  sf_layer_df[,"geom_text"]<-st_as_text(st_transform(sf_layer_geom,crs = crs_id)) # convert geometry format
   
   # write data frame to database
   dbWriteTable(conn = con_inter_spatial, 
-               Id(schema="dbo", table=layer_name), 
+               Id(schema=schema_name, 
+                  table=db_table_name), 
                overwrite= TRUE,
-               sf_spatialdataset_df %>% select(-geometry))
+               sf_layer_df %>% select(-geometry))
   
   # add new geometry column to database table
   dbSendQuery(conn = con_inter_spatial, 
-              statement = paste0("ALTER TABLE dbo.",layer_name," ADD geom2 geometry"))
+              statement = paste0("ALTER TABLE ",schema_name,".",db_table_name," ADD geom geometry"))
   
   # transform and copy existing text-based geometry into new geometry column
   dbSendQuery(conn = con_inter_spatial,
-              statement = paste0("UPDATE dbo.",layer_name," Set geom2 = geometry::STGeomFromText(geom,", as.character(crs_id),")"))
+              statement = paste0("UPDATE ", schema_name,".",db_table_name," Set geom = geometry::STGeomFromText(geom_text,", as.character(crs_id),")"))
   
   # drop text-based geometry column
   dbSendQuery(conn = con_inter_spatial, 
-              statement = paste0("ALTER TABLE dbo.",layer_name," DROP COLUMN geom"))
+              statement = paste0("ALTER TABLE ", schema_name,".",db_table_name," DROP COLUMN geom_text"))
   
-  # read table
-  sf_spatialdataset_db<-st_read(con_inter_spatial, 
-                                query = paste0("SELECT NAME, geom2.STAsBinary() AS geom FROM dbo.",layer_name))
+  # read table (geometry only)
+  sf_layer_db<-st_read(con_inter_spatial, 
+                       query = paste0("SELECT geom.STAsBinary() as geometry FROM ", schema_name,".",db_table_name))
   
-return(sf_spatialdataset_db)
+  return(sf_layer_db)
 }
-
-
